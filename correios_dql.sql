91cbe0e7d14b0dc798cb57ef1239ffd682fa589d
Henrique Caires <36646802+henrique77@users.noreply.github.com>
2018-05-02 08:36:25 -0300

    Add files via upload

--- correios_dql.sql
+++ correios_dql.sql
@@ -8,4 +8,4 @@ select l.descricao as 'Logradouro', b.nome as 'Bairro', CONCAT( c.nome,'/', e.si
 		
 select l.descricao as 'Logradouro', b.nome as 'Bairro', CONCAT( c.nome,'/', e.sigla) as 'Cidade/UF', l.cep as 'CEP'  
 	from estados as e inner join cidades as c on e.sigla=c.sigla_uf inner join bairros as b on c.id=cidade_id 
-		inner join logradouros as l on b.id=l.bairro_id where l.descricao like '%pesquisar_logradouro%' and l.cep like '%pesquisar_cep%';
+		inner join logradouros as l on b.id=l.bairro_id where l.descricao like '%pesquisar_logradouro%' or l.cep like '%pesquisar_cep%';
