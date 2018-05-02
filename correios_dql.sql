b2594cc8c4b72ce2c41705eae9d06e2e3f46c280
Henrique Caires <36646802+henrique77@users.noreply.github.com>
2018-05-01 21:09:27 -0300

    Add files via upload

--- correios_dql.sql
+++ correios_dql.sql
@@ -1,11 +1,11 @@
 select l.descricao as 'Logradouro', b.nome as 'Bairro', CONCAT( c.nome,'/', e.sigla) as 'Cidade/UF', l.cep as 'CEP' 
-	from estados as e left join cidades as c on e.sigla=c.sigla_uf left join bairros as b on c.id=cidade_id 
-		left join logradouros as l on b.id=l.bairro_id where l.cep like '%4582%';
+	from estados as e inner join cidades as c on e.sigla=c.sigla_uf inner join bairros as b on c.id=cidade_id 
+		inner join logradouros as l on b.id=l.bairro_id where l.cep like '%4582%';
 		
 select l.descricao as 'Logradouro', b.nome as 'Bairro', CONCAT( c.nome,'/', e.sigla) as 'Cidade/UF', l.cep as 'CEP'  
-	from estados as e left join cidades as c on e.sigla=c.sigla_uf left join bairros as b on c.id=cidade_id 
-		left join logradouros as l on b.id=l.bairro_id where l.descricao like '%Deputado%';
+	from estados as e inner join cidades as c on e.sigla=c.sigla_uf inner join bairros as b on c.id=cidade_id 
+		inner join logradouros as l on b.id=l.bairro_id where l.descricao like '%Deputado%';
 		
 select l.descricao as 'Logradouro', b.nome as 'Bairro', CONCAT( c.nome,'/', e.sigla) as 'Cidade/UF', l.cep as 'CEP'  
-	from estados as e left join cidades as c on e.sigla=c.sigla_uf left join bairros as b on c.id=cidade_id 
-		left join logradouros as l on b.id=l.bairro_id where l.descricao like '%pesquisar_logradouro%' and l.cep like '%pesquisar_cep%';
+	from estados as e inner join cidades as c on e.sigla=c.sigla_uf inner join bairros as b on c.id=cidade_id 
+		inner join logradouros as l on b.id=l.bairro_id where l.descricao like '%pesquisar_logradouro%' and l.cep like '%pesquisar_cep%';
